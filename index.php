9355b30fd3d4092ffb4f1666749c8f38e99d0838
lamiru <lamiru@gmail.com>
2019-04-08 20:45:15 +0900

    mobile publishing

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="ko">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
 <meta name="path_role" content="MAIN">
 <meta name="author" content="퓨처밸류21">
 <meta name="description" content="온라인광고, 비지니스매칭">
@@ -23,20 +24,108 @@
 <link rel="stylesheet" href="/static/css/common.css" type="text/css">
 </head>
 <body>
-<h2>Hello World</h2>
-<h3>성형병원 + 미용실</h3>
-<h3>자매 결연 마케팅 제안서</h3>
-<h4>성형병원 + 미용실 자매결연 마케팅 이란?</h4>
-<p>미용실과 성형병원을 자매 결연을 맺어 미용실 방문 손님에게 추천하는 시스템이며 주로 여성을 타겟으로 하는 강력한 홍보 플랫폼으로 병원 손님을 안정적으로 꾸준히 유치할 수 있습니다.</p>
-<h4>성형병원 미용실 자매 결연 마케팅 효과</h4>
-<p>미용실 원장 (원장 및 근무자)이 추천하는 병원이라 인터넷 가짜 후기 광고보다 신뢰가 높아 빠른 광고 효과를 얻을 수 있습니다.</p>
-<p>미용실을 마케팅 매개체로 활용하여 자연스럽게 성형에 대한 커뮤니티가 형성이 되며 아는 언니가 성형을 한 것을 보고 나고 싶고 싶다 라는 충동이 발생합니다.</p>
-<p>미용실이란 장점을 이용하여 매월 안정적으로 고객을 유치할 수 있습니다.</p>
-<p>미용실당 500원의 비용으로 광고를 진행할 수 있습니다. (미용실 방문객 1인 광고 단가 0.8원) 미용실 방문객 평균 최소 일일 20명(손님 지인 포함) 월 600명 미용실 1만곳 방문객 600만명에게 안정적인 마케팅이 가능합니다.</p>
-<p>미용실은 방문자 손님이 최소 2~3시간 머물러 있어야 하므로 성형병원을 어필할 수 있는 충분한 시간이 있습니다.</p>
-<p>기존 잡지책에 실린 성형병원 광고는 그냥 한 번 보고 지나가는 경우가 많습니다. 하지만 퓨처밸류21이 개발한 성형병원 전문 마케팅 플랫폼 시스템은 성형병원과 미용실을 자매 결연을 맺어 적극 입소문 마케팅하게 하는 시스템입니다.</p>
-<p>자매 결연을 맺음으로 미용실 원장은 특정 병원에 소속감을 갖게 됩니다. 본인이 운영하는 미용실 손님을 모으기 위해 미용실 입구 유리에 특정 성형외과와 자매 결연을 맺은 미용실이라고 포스터를 붙이며 손님이 보는 거울 하단에 성형병원 미니 브로셔를 배치합니다. 자연히 손님은 자매 결연을 맺은 성형병원에 대해 자연스럽게 이야기하게 되어 다른 인터넷 광고보다 더 신뢰하게 되어 효과 좋은 결과로 이어집니다.</p>
-<p>미용실 손님이 병원으로 오기까지 예를 들겠습니다. 40대 손님 A씨는 미용실에 머리를 하러 왔다가 지루한 시간에 거울 앞 성형병원 미니 브로셔를 보게 되며 미용실 직원(원장)과 성형에 관련된 다양한 이야기를 하게 됩니다. 사람 심리가 평소에 관심은 있었으나 접할 기회가 적어 마음 속에 있던 궁금한 내용들을 미용실 직원(원장)과 편하게 이야기하며 "어느 병원 괜찮을까요?" 이야기합니다. 그러던 중 추천한 병원으로 문의하게 되어 간단한 쌍꺼풀 수술 하려고 왔다가 아마 코 등 다양한 성형까지 하게 됩니다.</p>
-<p>성형병원과 미용실 자매 결연은 한 번 맺으면 장기적으로 마케팅이 유지가 되며 지속적인 마케팅 효과가 증폭되어 발생합니다.</p>
+<hr class="bc-ffd105 mt-0 border-width-6">
+<div class="pr-2 pl-2">
+  <div class="bg-1c2222 pt-3 pb-3 text-center">
+    <h3 class="color-acd80b m-1">성형병원 + 미용실</h3>
+    <h2 class="ml-0 mr-0 mt-1 mb-1">자매 결연 마케팅 <span class="fw-300">제안서</span></h2>
+  </div>
+  <hr class="bc-acd80b mt-3 border-width-2">
+  <div>
+    <div class="pull-left"><img src="https://dummyimage.com/60x70/000/fff"></div>
+    <h3 class="pull-left mt-1 pl-2">
+      <span class="fw-300 color-43e7d8">성형병원 + 미용실</span><br>
+      <span class="pl-2 color-43e7d8">자매결연 마케팅 </span>
+      <span class="fw-300">이란?</span>
+    </h3>
+    <div class="clearfix"></div>
+  </div>
+  <p class="p-3 pb-4 mb-4">미용실과 성형병원을 자매 결연을 맺어 미용실 방문 손님에게 추천하는 시스템이며 주로 여성을 타겟으로 하는 강력한 홍보 플랫폼으로 병원 손님을 안정적으로 꾸준히 유치할 수 있습니다.</p>
+  <hr class="bc-acd80b border-width-2 mb-0">
+  <h3 class="mt-4">
+    <span class="fs-30">01</span>
+  </h3>
+  <div class="mb-4">
+    <h2 class="pull-left pr-3">02</h2>
+    <h3 class="pull-left">
+      <span class="fw-300">성형병원 미용실 자매 결연</span><br>
+      <span class="color-43e7d8 fs-35">마케팅 효과</span>
+    </h3>
+    <div class="clearfix"></div>
+  </div>
+  <ul>
+    <li>미용실 원장 (원장 및 근무자)이 추천하는 병원이라 인터넷 가짜 후기 광고보다 신뢰가 높아 빠른 광고 효과를 얻을 수 있습니다.</li>
+    <li>미용실을 마케팅 매개체로 활용하여 자연스럽게 성형에 대한 커뮤니티가 형성이 되며<br>아는 언니가 성형을 한 것을 보고 나고 싶고 싶다 라는 <span class="color-acd80b fs-18 fw-600">충동이 발생합니다.</span></li>
+    <li>미용실이란 장점을 이용하여 매월 안정적으로 고객을 유치할 수 있습니다.</li>
+    <li><span class="fs-18 fw-600">미용실당 </span><span class="color-ffd105 fs-18 fw-600">500원 </span>의 비용으로 광고를 진행할 수 있습니다. ( <span class="color-acd80b">미용실 방문객 1인 광고 단가 </span><span class="color-ffd105">0.8원</span> ) 미용실 방문객 평균 최소 일일 20명(손님 지인 포함) 월 600명<br><span class="fs-18 fw-600">미용실 </span><span class="fs-18 fw-600 color-acd80b">1만곳 </span><span class="fs-18 fw-600">방문객 </span><span class="fs-18 fw-600 color-acd80b">600만명 </span>에게 안정적인 마케팅이 가능합니다.</li>
+    <li>미용실은 방문자 손님이 최소 2~3시간 머물러 있어야 하므로 성형병원을 어필할 수 있는 충분한 시간이 있습니다.</li>
+    <li>기존 잡지책에 실린 성형병원 광고는 그냥 한 번 보고 지나가는 경우가 많습니다.<br>하지만 <span class="fs-18 fw-600 color-43e7d8">퓨처밸류21 </span>이 개발한 성형병원 전문 마케팅 플랫폼 시스템은 성형병원과 미용실을 자매 결연을 맺어 적극 입소문 마케팅하게 하는 시스템입니다.</li>
+    <li>자매 결연을 맺음으로 미용실 원장은 특정 병원에 소속감을 갖게 됩니다.<br>본인이 운영하는 미용실 손님을 모으기 위해 미용실 입구 유리에 특정 성형외과와 자매 결연을 맺은 미용실이라고 포스터를 붙이며 손님이 보는 거울 하단에 성형병원 미니 브로셔를 배치합니다.<br>자연히 손님은 자매 결연을 맺은 성형병원에 대해 자연스럽게 이야기하게 되어 다른 인터넷 광고보다 더 신뢰하게 되어 효과 좋은 결과로 이어집니다.</li>
+    <li class="color-acd80b">미용실 손님이 병원으로 오기까지 예를 들겠습니다. 40대 손님 A씨는 미용실에 머리를 하러 왔다가 지루한 시간에 거울 앞 성형병원 미니 브로셔를 보게 되며 미용실 직원(원장)과 성형에 관련된 다양한 이야기를 하게 됩니다.<br>사람 심리가 평소에 관심은 있었으나 접할 기회가 적어 마음 속에 있던 궁금한 내용들을 미용실 직원(원장)과 편하게 이야기하며 "어느 병원 괜찮을까요?" "아는데 있는데 소개해 드릴게요~" 등등 이야기합니다.<br>그러던 중 추천한 병원으로 문의하게 되어 간단한 쌍꺼풀 수술 하려고 왔다가 이마 코 등 다양한 성형까지 하게 됩니다.</li>
+    <li>성형병원과 미용실 자매 결연은 한 번 맺으면 장기적으로 마케팅이 유지가 되며 지속적인 마케팅 효과가 증폭되어 발생합니다.</li>
+  </ul>
+</div>
+<div class="bg-ffd105 pl-2 pr-2">
+  <div class="pull-left fs-18 color-1c2222 fw-600 p-2">광고상담 ＞ 02. 987. 7777</div>
+  <a href="tel:02-987-7777" class="btn btn-call d-block pull-left bg-1c2222 color-fff">통화연결</a>
+  <div class="clearfix"></div>
+</div>
+<div class="pr-2 pl-2">
+  <div class="bg-1c2222 mt-4 pt-3 pb-3 text-center">
+    <h2 class="ml-0 mr-0 mt-1 mb-1"><span class="fw-300">미용실 자매 결연 수</span><br>경쟁 비교</h2>
+  </div>
+  <div class="bg-1c2222 mt-4 pt-3 pb-3 text-center color-43e7d8">
+    <h4 class="ml-0 mr-0 mt-1 mb-1">전국 미용실 수는 <span class="color-fff">한정</span>되어 있습니다.</h4>
+    <p>경쟁력을 생각한다면 늦기 전에<br>미용실 확보를 최우선해야 합니다.</p>
+  </div>
+  <img class="mx-auto" src="https://dummyimage.com/300x280/000/fff">
+</div>
+<div class="bg-ffd105 pl-2 pr-2">
+  <div class="pull-left fs-18 color-1c2222 fw-600 p-2">광고상담 ＞ 02. 987. 7777</div>
+  <a href="tel:02-987-7777" class="btn btn-call d-block pull-left bg-1c2222 color-fff">통화연결</a>
+  <div class="clearfix"></div>
+</div>
+<div class="pr-2 pl-2">
+  <div class="bg-1c2222 mt-4 pt-3 pb-3 text-center">
+    <h2 class="ml-0 mr-0 mt-1 mb-1"><span class="fw-300">미용실 홍보 책자</span><br>제작 및 배포 <span class="fw-300">안내</span></h2>
+  </div>
+  <img class="w-100 mb-3" src="/static/img/process.png" alt="Process">
+</div>
+<div class="bg-ffd105 pl-2 pr-2">
+  <div class="pull-left fs-18 color-1c2222 fw-600 p-2">광고상담 ＞ 02. 987. 7777</div>
+  <a href="tel:02-987-7777" class="btn btn-call d-block pull-left bg-1c2222 color-fff">통화연결</a>
+  <div class="clearfix"></div>
+</div>
+<div class="pr-2 pl-2">
+  <div class="bg-1c2222 mt-4 pt-3 pb-3 text-center">
+    <h2 class="ml-0 mr-0 mt-1 mb-1">미용실 마케팅<br><span class="fw-300">진행 예상도</span></h2>
+  </div>
+  <div class="bc-acd80b border-1 border-b-0 height-30 mt-5"></div>
+  <div class="bc-acd80b border-1 border-t-0 height-30 mb-5"></div>
+</div>
+<div class="bg-ffd105 pl-2 pr-2">
+  <div class="pull-left fs-18 color-1c2222 fw-600 p-2">광고상담 ＞ 02. 987. 7777</div>
+  <a href="tel:02-987-7777" class="btn btn-call d-block pull-left bg-1c2222 color-fff">통화연결</a>
+  <div class="clearfix"></div>
+</div>
+<div class="pr-2 pl-2">
+  <div class="bg-1c2222 mt-4 pt-3 pb-3 text-center">
+    <h2 class="ml-0 mr-0 mt-1 mb-1">광고 진행 순서</h2>
+  </div>
+  <h4 class="color-acd80b text-center mb-2">1차 국내 미용실 마케팅 종료 후</h4>
+  <h4 class="color-acd80b text-center mb-4">2차 중국 미용실 마케팅 진행 예정입니다.</h4>
+</div>
+<hr class="bc-ffd105 border-width-4">
+<div class="pr-2 pl-2">
+  <h3 class="ml-0 mr-0 mt-1 mb-1 text-center">홍보 책자 지면 비용 안내</h3>
+  <hr class="bc-acd80b mt-3 border-width-2">
+  <hr class="bc-acd80b mt-3 border-width-2">
+  <p>성형 병원 + 자매 결연 마케팅 서비스는 자매 결연 맺을 미용실 대상 성형 병원 책자 1만부 인쇄 및 배포까지 서비스를 제공하며 미용실 1만 곳 배포 후 자매 결연 맺을 시 고정 성형 병원 브로셔 및 포스터 제작 인쇄 배포 비용 및 관리비는 별도입니다.</p>
+</div>
+<div class="bg-ffd105 pl-2 pr-2">
+  <div class="pull-left fs-18 color-1c2222 fw-600 p-2">광고상담 ＞ 02. 987. 7777</div>
+  <a href="tel:02-987-7777" class="btn btn-call d-block pull-left bg-1c2222 color-fff">통화연결</a>
+  <div class="clearfix"></div>
+</div>
 </body>
 </html>
