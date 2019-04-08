0c18c7aa4a5a8c664b13846aa45b3d0fae8b594f
lamiru <lamiru@gmail.com>
2019-04-08 16:19:53 +0900

    start project

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="ko">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="path_role" content="MAIN">
+<meta name="author" content="퓨처밸류21">
+<meta name="description" content="온라인광고, 비지니스매칭">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="keywords" content="홈페이지제작, 프로그램개발, 사이트제작, 앱개발, 어플리케이션제작, 앱제작, 웹에이전시">
+<meta property="og:type" content="website">
+<meta property="og:title" content="퓨처밸류21">
+<meta property="og:url" content="http://fvfv21.hubweb.net/">
+<meta property="og:description" content="온라인광고, 비지니스매칭 퓨처밸류21">
+</head>
+<body>
+<h2>Hello World</h2>
+</body>
+</html>
