ba6bef45842b021ab24fa3b46e68164672430863
lamiru <lamiru@gmail.com>
2019-04-08 17:24:42 +0900

    edit index.php

--- index.php
+++ index.php
@@ -6,13 +6,37 @@
 <meta name="author" content="퓨처밸류21">
 <meta name="description" content="온라인광고, 비지니스매칭">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="keywords" content="홈페이지제작, 프로그램개발, 사이트제작, 앱개발, 어플리케이션제작, 앱제작, 웹에이전시">
+<meta name="keywords" content="성형외과, 미용실, 홈페이지제작, 프로그램개발, 사이트제작, 앱개발, 어플리케이션제작, 앱제작, 웹에이전시">
 <meta property="og:type" content="website">
 <meta property="og:title" content="퓨처밸류21">
 <meta property="og:url" content="http://fvfv21.hubweb.net/">
 <meta property="og:description" content="온라인광고, 비지니스매칭 퓨처밸류21">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+<!--[if lt IE 9]>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.1/jquery.min.js"></script>
+<script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<!--[if (gte IE 9) | (!IE)]><!-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!--<![endif]-->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="/static/css/common.css" type="text/css">
 </head>
 <body>
 <h2>Hello World</h2>
+<h3>성형병원 + 미용실</h3>
+<h3>자매 결연 마케팅 제안서</h3>
+<h4>성형병원 + 미용실 자매결연 마케팅 이란?</h4>
+<p>미용실과 성형병원을 자매 결연을 맺어 미용실 방문 손님에게 추천하는 시스템이며 주로 여성을 타겟으로 하는 강력한 홍보 플랫폼으로 병원 손님을 안정적으로 꾸준히 유치할 수 있습니다.</p>
+<h4>성형병원 미용실 자매 결연 마케팅 효과</h4>
+<p>미용실 원장 (원장 및 근무자)이 추천하는 병원이라 인터넷 가짜 후기 광고보다 신뢰가 높아 빠른 광고 효과를 얻을 수 있습니다.</p>
+<p>미용실을 마케팅 매개체로 활용하여 자연스럽게 성형에 대한 커뮤니티가 형성이 되며 아는 언니가 성형을 한 것을 보고 나고 싶고 싶다 라는 충동이 발생합니다.</p>
+<p>미용실이란 장점을 이용하여 매월 안정적으로 고객을 유치할 수 있습니다.</p>
+<p>미용실당 500원의 비용으로 광고를 진행할 수 있습니다. (미용실 방문객 1인 광고 단가 0.8원) 미용실 방문객 평균 최소 일일 20명(손님 지인 포함) 월 600명 미용실 1만곳 방문객 600만명에게 안정적인 마케팅이 가능합니다.</p>
+<p>미용실은 방문자 손님이 최소 2~3시간 머물러 있어야 하므로 성형병원을 어필할 수 있는 충분한 시간이 있습니다.</p>
+<p>기존 잡지책에 실린 성형병원 광고는 그냥 한 번 보고 지나가는 경우가 많습니다. 하지만 퓨처밸류21이 개발한 성형병원 전문 마케팅 플랫폼 시스템은 성형병원과 미용실을 자매 결연을 맺어 적극 입소문 마케팅하게 하는 시스템입니다.</p>
+<p>자매 결연을 맺음으로 미용실 원장은 특정 병원에 소속감을 갖게 됩니다. 본인이 운영하는 미용실 손님을 모으기 위해 미용실 입구 유리에 특정 성형외과와 자매 결연을 맺은 미용실이라고 포스터를 붙이며 손님이 보는 거울 하단에 성형병원 미니 브로셔를 배치합니다. 자연히 손님은 자매 결연을 맺은 성형병원에 대해 자연스럽게 이야기하게 되어 다른 인터넷 광고보다 더 신뢰하게 되어 효과 좋은 결과로 이어집니다.</p>
+<p>미용실 손님이 병원으로 오기까지 예를 들겠습니다. 40대 손님 A씨는 미용실에 머리를 하러 왔다가 지루한 시간에 거울 앞 성형병원 미니 브로셔를 보게 되며 미용실 직원(원장)과 성형에 관련된 다양한 이야기를 하게 됩니다. 사람 심리가 평소에 관심은 있었으나 접할 기회가 적어 마음 속에 있던 궁금한 내용들을 미용실 직원(원장)과 편하게 이야기하며 "어느 병원 괜찮을까요?" 이야기합니다. 그러던 중 추천한 병원으로 문의하게 되어 간단한 쌍꺼풀 수술 하려고 왔다가 아마 코 등 다양한 성형까지 하게 됩니다.</p>
+<p>성형병원과 미용실 자매 결연은 한 번 맺으면 장기적으로 마케팅이 유지가 되며 지속적인 마케팅 효과가 증폭되어 발생합니다.</p>
 </body>
 </html>
